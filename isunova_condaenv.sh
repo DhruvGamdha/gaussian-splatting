d5f1e91bad7bf596c1f5673302ebcbe7b33f1897
nova-dgamdha <dgamdha@iastate.edu>
2025-03-17 12:54:56 -0500

    add conda environment setup script and training script; update .gitignore

---
+++ isunova_condaenv.sh
@@ -0,0 +1,15 @@
+# ----------
+# Required modules:
+# This set has python 3.9.18
+# default environment.yml requires python 3.7.13
+module purge
+module load micromamba
+module load cuda/11.8.0-zg46pdv
+# module load cuda/11.8.0-fvsgo54   # This creates a conflict with the micromamba module
+# ^^^^
+
+# ----------
+# conda env create --file environment.yml --prefix /work/mech-ai-scratch/dgamdha/conda_env/envs/gaussian_splatting
+eval "$(micromamba shell hook --shell=bash)"
+micromamba activate /work/mech-ai-scratch/dgamdha/conda_env/envs/gaussian_splatting
+# ^^^^
---
+++ start_train.sh
@@ -0,0 +1,4 @@
+# first run conda_env_activate.sh to load modules and activate the environment
+# # ----------
+
+python train.py -s /work/mech-ai-scratch/dgamdha/projects/sdat/code/gaussian-splatting/data/2025_jan_14/vid_1
