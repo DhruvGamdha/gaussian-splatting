d682a5a8f1964185c6305b47e21a6f8efe73bfaf
nova-dgamdha <dgamdha@iastate.edu>
2025-03-17 16:25:59 -0500

    update scripts to source the conda environment activation script before running conversion and training

--- run_convert.sh
+++ run_convert.sh
@@ -1,4 +1,4 @@
 # first run conda_env_activate.sh to load modules and activate the environment
 # # ----------
-
+source ./isunova_condaenv.sh
 python convert.py -s /work/mech-ai-scratch/dgamdha/projects/sdat/code/gaussian-splatting/data/2025_jan_14/vid_1
