f50ca8e837ccfade8e871dc22f14e9975b538faa
nova-dgamdha <dgamdha@iastate.edu>
2025-04-02 08:19:27 -0500

    WIP: testing new updated python and pytorch

---
+++ newNOVA_Mods_test.sh
@@ -0,0 +1,13 @@
+
+module load micromamba
+module load python/3.11.9-i2aasxp
+module load cuda/12.4.1-cz3ljd3
+
+
+# ----------
+# conda env create --file environment.yml --prefix /work/mech-ai-scratch/dgamdha/conda_env/envs/gaussian_splatting
+# eval "$(micromamba shell hook --shell=bash)"
+# micromamba activate /work/mech-ai-scratch/dgamdha/conda_env/envs/gaussian_splatting
+# micromamba create --file environment.yml --prefix /work/mech-ai-scratch/dgamdha/conda_env/envs/gaussian_splatting_v2
+# micromamba activate /work/mech-ai-scratch/dgamdha/conda_env/envs/gsplat_py38
+# ^^^^
